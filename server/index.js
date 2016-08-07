e21b91d43e50a71f07da835d97ad4911f7e35a31
Brandon Thomas <lucky7id@gmail.com>
2016-08-07 01:49:15 -0700

    bind ctrl functions properly, update git ignore for dist dir

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app
     .get('/', (req, res) => {
         res.send('hello world');
     })
-    .get('/jobs', jobsCtrl.getJobs)
+    .get('/jobs', jobsCtrl.getJobs.bind(jobsCtrl))
     .listen(3001, () => {
         console.log('Example app listening on port 3000!');
     });
--- server/jobs.js
+++ server/jobs.js
@@ -5,17 +5,16 @@ const getAdminJobsQuery = `SELECT * from job_postings WHERE status = 'active' or
 export default class JobsController {
     constructor (db) {
         this.db = db;
-        console.log(db);
     }
 
     getJobs (req, res) {
-        console.log(this.db);
         this.db.query(getJobsQuery, (err, rows, fields) => {
             this.db.end();
 
             if (err) res.json({success: false, error: err});
-            console.log(rows, fields);
-            res.json({rows: rows, fields: fields})
+
+            console.log(`GET request to jobs unathenticated returned ${rows.length} rows`);
+            res.json({items: rows});
         });
     }
 }
